d3c10731976176d92b7b7d612d6410ea9b388276
VladislavHub <darkdevil9592@gmail.com>
2023-09-05 17:39:07 +0300

    done

--- index.js
+++ index.js
@@ -17,14 +17,25 @@
  */
 
 // Створюємо об'єкт Book
+const Book = {
+    title: "Загальна Книга",
+    author: "Анонім",
+    pages: 0,
+    read() {console.log(`Ви читаєте "${this.title}" від ${this.author}`)}
+}
 
 console.log("Завдання: 1 ==============================");
 
 // Виводимо в консоль Об'єкт: Book
+console.log(Book)
 
 // Виводимо в консоль прототип Об'єкту: Book
+const prototype = Object.create(Book)
+console.log(Object.getPrototypeOf(prototype) === Book)
 
 // Викликаємо функцію read об'єкту Book
+Book.read()
+
 
 // 2. Наслідування від базового об'єкту Book
 
@@ -38,14 +49,18 @@ console.log("Завдання: 1 ==============================");
  */
 
 // Створюємо об'єкт Novel, наслідуємо властивості і функції від об'єкта Book
+const Novel = Object.create(Book)
 
 // Додаємо властивість genre
+Novel.ganre = "Новела"
 
 console.log("Завдання: 2 ==============================");
 
 // Виводимо в консоль Об'єкт: Novel
+console.log(Novel)
 
 // Виводимо в консоль прототип Об'єкту: Novel
+console.log(Object.getPrototypeOf(Novel))
 
 // 3. Створення нового об'єкту та зміна його прототипу
 
@@ -61,13 +76,21 @@ console.log("Завдання: 2 ==============================");
  */
 
 // Створюємо об'єкт Biography
+const Biography = {
+    title: "Загальна Біографія",
+    author: "Біограф",
+    pages: 200,
+}
 
 // Змінемо прототип об'єкта Biography на Novel
+Object.setPrototypeOf(Novel, Biography)
 
 console.log("Завдання: 3 ==============================");
 // Виводимо в консоль Об'єкт: Biography
+console.log(Biography)
 
 // Перевіримо чи являється Novel прототипом Biography та виведемо в консоль
+console.log(Object.getPrototypeOf(Novel) === Biography)
 
 // 4. Інкапсуляція властивості та додання властивості
 /*
@@ -94,10 +117,31 @@ console.log("Завдання: 3 ==============================");
 // | author      | "Альберт Ейнштейн"   |
 // | info        | написана в 1915 році |
 
+const ScienceBook = Object.create(Book)
+
+Object.defineProperty(ScienceBook, 'info', {
+    configurable: false,
+    set(value){
+        this._info = value 
+    },
+
+    get(){
+        return `Про книгу "${this.title}": ${this._info}`
+    },
+})
+
+ScienceBook.title = "Фізика 101"
+ScienceBook.author = "Альберт Ейнштейн"
+ScienceBook.info = "написана в 1915 році"
+
+// ScienceBook.info = "test"
+
 console.log("Завдання: 4 ==============================");
 // Виводимо в консоль властивість info
+console.log(ScienceBook.info)
 
 // Виводимо в консоль налаштування властивости info
+console.log(Object.getOwnPropertyDescriptor(ScienceBook, 'info'))
 
 // 5. Поліморфізм: створення нового об'єкта та перевизначення його методу
 /*
@@ -117,8 +161,16 @@ console.log("Завдання: 4 ==============================");
 // | title       | "Фізика у Вищій Школі"     |
 // | author      | "Дж. Д. Джонс"             |
 
+const Textbook = Object.create(ScienceBook)
+
+Textbook.read = function () {console.log(`Ви читаєте підручник "${this.title}" від ${this.author}. ${this.info}`)}
+
+Textbook.title = "Фізика у Вищій Школі" 
+Textbook.author = "Дж. Д. Джонс" 
+
 console.log("Завдання: 5 ==============================");
 // Викликаємо функцію read об'єкту Textbook
+Textbook.read()
 
 // 6. Абстракція: створення об'єкта з загальними властивостями
 /*
@@ -138,6 +190,11 @@ console.log("Завдання: 5 ==============================");
  */
 
 // Створюємо об'єкт Media
+const Media = {
+    format: "Загальний Формат",
+    length: 0,
+    play() {console.log(`Зараз відтворюється медіа у форматі ${this.format} з тривалістю ${this.length} секунд`)}
+}
 
 /*
  * Об'єкт: Song
@@ -152,6 +209,11 @@ console.log("Завдання: 5 ==============================");
 // |-------------|------------------------|
 // | artist      | "Загальний Виконавець" |
 // | title       | "Загальна Пісня"       |
+const Song = Object.create(Media)
+
+Song.artist = "Загальний Виконавець"
+Song.title = "Загальна Пісня"
 
 console.log("Завдання: 6 ==============================");
 // Викликаємо функцію play об'єкту Song
+Song.play()
